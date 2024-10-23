334a3f2c1b0d8009d6fc08a2394e8329520981e7
haiphan <hai.phan@chotot.vn>
2024-10-23 13:33:16 +0700

    [Revenue][CRE-]

---
+++ checkcicd2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checkcicd2
+//
+//  Created by Hai Phan Thanh on 23/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
